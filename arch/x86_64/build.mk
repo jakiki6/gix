eae963422a05911407bffd1c25d449819c8bb2f0
Jakob Kirsch <jakob.kirsch@web.de>
2022-11-25 00:08:21 +0100

    x86_64: fix build.mk

--- arch/x86_64/build.mk
+++ arch/x86_64/build.mk
@@ -2,29 +2,23 @@ NASMFLAGS = -F dwarf -g -f elf64
 CFLAGS += -m64 -march=x86-64 -mabi=sysv -mno-80387 -mno-mmx -mno-sse -mno-sse2 -mno-red-zone -mcmodel=kernel -MMD
 LDFLAGS += -m elf_x86_64 -z max-page-size=0x1000 -T arch/$(ARCH)/linker.ld
 
-
 override CFILES := $(shell find kernel -type f -name '*.c') $(shell find arch/$(ARCH) -type f -name '*.c')
 override ASFILES := $(shell find arch/$(ARCH) -type f -name '*.S')
 override NASMFILES := $(shell find arch/$(ARCH) -type f -name '*.asm')
 override OBJ := $(CFILES:.c=.o) $(ASFILES:.S=.o) $(NASMFILES:.asm=.o)
 override HEADER_DEPS := $(CFILES:.c=.d) $(ASFILES:.S=.d)
 
-# Link rules for the final kernel executable.
 $(KERNEL): $(OBJ)
 	$(LD) $(OBJ) $(LDFLAGS) -o $@
 
-# Include header dependencies.
 -include $(HEADER_DEPS)
 
-# Compilation rules for *.c files.
 %.o: %.c
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
 
-# Compilation rules for *.S files.
 %.o: %.S
 	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
 
-# Compilation rules for *.asm (nasm) files.
 %.o: %.asm
 	nasm $(NASMFLAGS) $< -o $@
 
@@ -40,7 +34,6 @@ $(ARCH)_debug: boot.img
 	gdb -x debug.gdb
 	killall qemu-system-x86_64
 
-# Remove object files and the final executable.
 .PHONY: $(ARCH)_clean
 $(ARCH)_clean:
-	rm -rf $(OBJ) $(HEADER_DEPS) os.img
+	rm -rf $(OBJ) $(HEADER_DEPS) boot.img
